3b7252dd1e70f967381839effc4fa01ce74113f8
Walid Amriou <walidamriou@gmail.com>
2020-07-26 07:20:37 +0200

    add wifi part

--- Example/ESP32_OTA_Update_via_HTTPS/src/main.c
+++ Example/ESP32_OTA_Update_via_HTTPS/src/main.c
@@ -1,1 +1,9 @@
-void app_main() {}
+#include <wifi.h>
+#include <esp_system.h>
+
+void app_main() {
+    wifi_init();
+    printf("wifi init done.\n");
+    wifi_connect();
+    printf("Connected to wifi network\n");
+}
