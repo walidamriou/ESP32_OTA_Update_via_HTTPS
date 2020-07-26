aeb2234e41cb163aa61dd846d7ac6afb2addfe9f
Walid Amriou <walidamriou@gmail.com>
2020-07-26 22:03:41 +0200

    Update Example/ESP32_OTA_Update_via_HTTPS/src/main.c

--- Example/ESP32_OTA_Update_via_HTTPS/src/main.c
+++ Example/ESP32_OTA_Update_via_HTTPS/src/main.c
@@ -1,9 +1,22 @@
+/* 
+Connecting to an WiFi Access Point 
+Developed by: Walid Amriou
+www.walidamriou.com
+
+Last update: July 2020
+*/
+
 #include <wifi.h>
+#include <update.h>
 #include <esp_system.h>
 
 void app_main() {
+    printf("Firmware2\n");
     wifi_init();
     printf("wifi init done.\n");
     wifi_connect();
     printf("Connected to wifi network\n");
+
+
+    
 }
