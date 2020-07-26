e308ac8e63989ccf9cae77000545415dac561fbf
Walid Amriou <walidamriou@gmail.com>
2020-07-26 18:26:38 +0200

    add Example/ESP32_OTA_Update_via_HTTPS/src/update_info.h

---
+++ Example/ESP32_OTA_Update_via_HTTPS/src/update_info.h
@@ -0,0 +1,10 @@
+/* 
+Connecting to an WiFi Access Point 
+Developed by: Walid Amriou
+www.walidamriou.com
+
+Last update: July 2020
+*/
+
+#define FIRMWARE_VERSION	1.0
+#define UPDATE_JSON_URL		"http://www.walidamriou.com/ESP32_OTA_Update_via_HTTPS/Example/ESP32_OTA_Update_via_HTTPS/src/firmware_status.json"
