ac20de9e9bebafc715cdc49e1925010c4f24df21
Walid Amriou <walidamriou@gmail.com>
2020-07-27 20:51:32 +0200

    remove import ssl certificate

--- Example/ESP32_OTA_Update_via_HTTPS/src/update.h
+++ Example/ESP32_OTA_Update_via_HTTPS/src/update.h
@@ -9,10 +9,6 @@ Last update: July 2020
 #ifndef UPDATE_H_INCLUDED
 #define UPDATE_H_INCLUDED
 
-// server certificates
-extern const char server_cert_pem_start[] asm("_binary_certs_pem_start");
-extern const char server_cert_pem_end[] asm("_binary_certs_pem_end");
-
 // receive buffer
 char rcv_buffer[200];
 
