bffd92e72a19a0507e911b995ea920ba8ed2c63e
Walid Amriou <walidamriou@gmail.com>
2020-07-28 01:05:38 +0200

    add wifi_info.h

---
+++ Example/ESP32_OTA_Update_via_HTTPS/src/wifi_info.h
@@ -0,0 +1,15 @@
+/* 
+Connecting to an WiFi Access Point 
+Developed by: Walid Amriou
+www.walidamriou.com
+
+Last update: July 2020
+*/
+#ifndef WIFI_INFO_H_INCLUDED
+#define WIFI_INFO_H_INCLUDED
+
+#define wifi_name	"wifi_username"
+#define wifi_password "wifi_password"
+
+#endif
+
