974ab955ffaf16794d989b49fd0f7af2f4f29b96
Walid Amriou <walidamriou@gmail.com>
2020-07-28 00:43:03 +0200

    Update update_info.h

--- Example/ESP32_OTA_Update_via_HTTPS/src/update_info.h
+++ Example/ESP32_OTA_Update_via_HTTPS/src/update_info.h
@@ -6,5 +6,5 @@ www.walidamriou.com
 Last update: July 2020
 */
 
-#define FIRMWARE_VERSION	1.0
+#define FIRMWARE_VERSION	2.0
 #define FIRMWARE_STATUS_JSON_URL "http://www.walidamriou.com/ESP32_OTA_Update_via_HTTPS/Example/ESP32_OTA_Update_via_HTTPS/src/firmware_status.json"
