f016f60b048d5929f7979551f4877e4cabd1508b
Walid Amriou <walidamriou@gmail.com>
2020-07-28 01:18:14 +0200

    Update src/main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 /* 
-Connecting to an WiFi Access Point 
+ESP32 OTA Update via HTTPS 
+Implement Esperssif ESP32 OTA (Over-The-Air) updates via HTTPS with the ESP-IDF framework.
 Developed by: Walid Amriou
 www.walidamriou.com
+
+This work is licensed under:
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
 
 Last update: July 2020
 */
